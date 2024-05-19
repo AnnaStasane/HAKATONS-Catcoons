04f719ac5c71f4ed70504d5084ae363a712da8c0
AnnaStasane <123621667+AnnaStasane@users.noreply.github.com>
2024-05-19 13:10:18 +0300

    Add files via upload
    Atjaunoju kodu, lai būtu spēlētājam skaidrojums

--- HAKATON.py
+++ HAKATON.py
@@ -1,11 +1,11 @@
-import pygame, sys, random
+import pygame, sys, random, time
 pygame.init()
 
-WIDTH, HEIGHT = 1000, 600
+WIDTH, HEIGHT = 900, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("HAKATONS PYGIRLS")
 
-goodchoice_img = pygame.image.load("img/good_img.png") #vizuāli atspoguļojumi izvēlēm
+goodchoice_img = pygame.image.load("img/good_img.png")
 badchoice_img = pygame.image.load("img/bad_img.png")
 
 sum = 0
@@ -25,10 +25,11 @@ city_image = {
     -5:pygame.image.load("img/red5.png")
 }
 
-stand_img = pygame.image.load("img/stand.png") #maziņš personāžs, kas stāv uz podiuma
+stand_img = pygame.image.load("img/stand.png")
 
 font = pygame.font.SysFont(None, 100)
 text_font = pygame.font.SysFont(None, 30)
+start_font = pygame.font.SysFont(None, 40)
 
 good_text = [
     'Piešķirt lielāku finansējumu skolām?',
@@ -39,23 +40,43 @@ good_text = [
     'Ieviest lielāku finansējumu ceļu uzlabošanā un drošībā?',
     'Iekārtot vairāk jauniešiem draudzīgas vietas?',
     'Iekārtot bērniem un ģimenēm draudzīgas vides?',
-    'Piešķirt lielaku atbalstu bezpajumtniekiem?'
+    'Piešķirt lielaku atbalstu bezpajumtniekiem un daudzbērnu ģimenēm?'
     ]
 bad_text = [
-    'Atļaut mest atkritumus parkos?',
-    'Aizstāt parkus ar dzīvokļiem un rūpnīcām?',
-    'Ļaut rūpnīcām mest izmešus pilsētas upēs un ūdenstilpnēs?', 
+    'Noņemt atkritumu tvertnes parkos kopējās ainavas uzlabošanai?',
+    'Zaļajās zonās būvēt jaunas dzīvojamās ēkas un rūpnīcas?',
+    'Ļaut rūpnīcās radušos izmešus izskalot pilsētas ūdenstilpēs?', 
     'Atņemt finansējumu kultūras, mākslas un izklaides sektoram?',
     'Aizliegt mājdzīvnieku klātbūtni parkos?',
-    'Atdot maztrucīgajiem plānoto finansējumu Valdības pārstāvju algu palielināšanai?',
+    'Novirzīt valsts budžeta finanses valdības algas palielināšanai?',
     'Neregulēt gaisa piesārņojuma pārkāpumus?',
-    'Nefinansēt ceļu uzlabošanu?',
-    'Palielināt nodokļu likmes mājokļiem un elektrībai?'
+    'Ceļu labošanu atstāt pašvaldību apgādībā?',
+    'Palielināt nodokļu likmes?'
     ]
 
 good_phrase = random.choice(good_text)
 bad_phrase = random.choice(bad_text)
 hover_text = ''
+
+start_text = [
+    "Sveiks, spēlētāj!",
+    "Tu esi guvis piekļuvi Rīgas potenciālajiem pilsētas plāniem.",
+    "Tev 5 mēģinājumu laikā jāizvēlas starp 2 opcijām.",
+    "Have fun!"
+]
+
+start_rect_height = 160
+start_rect = pygame.Surface((WIDTH, start_rect_height), pygame.SRCALPHA)
+start_rect.fill((0, 0, 0, 128))
+
+line_height = 40
+for i, line in enumerate(start_text):
+    start_message = start_font.render(line, True, (255, 255, 255))
+    start_rect.blit(start_message, (WIDTH // 2 - start_message.get_width() // 2, i * line_height + 10))
+
+start_time = time.time()
+display_time = 1 * 60
+show_text = True
 
 run = True
 while run:
@@ -75,16 +96,23 @@ while run:
         stand_img = pygame.image.load("img/stand.png")
         hover_text = ''
 
-    #new textbox
     pygame.draw.rect(screen, (255,255,255), (0, HEIGHT - 50, WIDTH, 50))
     text_surface = text_font.render(hover_text, True, (0, 0, 0))
     screen.blit(text_surface, (10, HEIGHT - 40))
+
+    if show_text:
+        screen.blit(start_rect, (0, 50))
+        if time.time() - start_time >= display_time:
+            show_text = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if show_text:
+                show_text = False
+
             if goodchoice.collidepoint(pygame.mouse.get_pos()):
                 sum += 1
                 turns += 1
@@ -96,7 +124,6 @@ while run:
                 good_phrase = random.choice(good_text)
                 bad_phrase = random.choice(bad_text)
                 
-
     if turns == 5:
         screen.blit(city_image[sum], (0, 0))
         end_text = font.render("Game end!", True, (255, 255, 255))
